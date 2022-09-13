9ffcd16cc4e2d0de17114787ce5aee1cddacd246
Ancel-Ishita <110010429+Ancel-Ishita@users.noreply.github.com>
2022-09-13 11:43:44 +0530

    Sixth commit

--- Main.py
+++ Main.py
@@ -1,20 +1,23 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
-    for n in range(1,len(array)):
-        key = array[n]
-        num = n-1
-        while num >= 0 and key < array[num]:
-            array[num + 1] = array[num]
-            num -= 1
-        array[num + 1] = key
-    return array
-# data = [9, 5, 1, 4, 3]
+  # Write your code here
+   for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1     
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        
+        array[j + 1] = key
+   return array
+
+data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
-    data.append(int(item
+    data.append(int(item))
 print(insertionSort(data))
